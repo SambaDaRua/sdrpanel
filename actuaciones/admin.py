1195e24214b55d269d63635f285176f98b2b2cf6
Pablo Catalina <pcatalin@cisco.com>
2019-01-22 11:03:51 +0100

    tabs to spaces

--- actuaciones/admin.py
+++ actuaciones/admin.py
@@ -2,13 +2,13 @@ from actuaciones.models import samberos, contactos, instrumentos, actuaciones
 from django.contrib import admin
 
 class actuacionesAdmin(admin.ModelAdmin):
-	exclude = ['samberos']
-	fields = ['titulo', 'fecha', 'descripcion', 'lugar', 'organizador', 'contacto', 'confirmada']
-	list_display = ('fecha', 'titulo', 'lugar', 'confirmada')
+    exclude = ['samberos']
+    fields = ['titulo', 'fecha', 'descripcion', 'lugar', 'organizador', 'contacto', 'confirmada']
+    list_display = ('fecha', 'titulo', 'lugar', 'confirmada')
 class samberosAdmin(admin.ModelAdmin):
-	exclude = ['user_permissions', 'groups', 'last_login', 'date_joined']
-	list_display = ('username', 'first_name', 'last_name', 'email', 'movil', 'instrumento', 'is_superuser', 'backstage','is_active')
-	list_filter = ('instrumento', 'is_active', 'backstage')
+    exclude = ['user_permissions', 'groups', 'last_login', 'date_joined']
+    list_display = ('username', 'first_name', 'last_name', 'email', 'movil', 'instrumento', 'is_superuser', 'backstage','is_active')
+    list_filter = ('instrumento', 'is_active', 'backstage')
 
 
 admin.site.register(contactos)
