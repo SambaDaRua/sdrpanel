9d0778618d03292a397c619ab489e36e986ad7b3
Pablo Catalina <pcatalin@cisco.com>
2019-01-23 15:17:22 +0100

    Added password reset

--- sdrpanel/urls.py
+++ sdrpanel/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
         url('^admin/', include(admin.site.urls)),
         url(r'^password_reset/$', password_reset, name='password_reset'),
         url(r'^password_reset/done/$', password_reset_done, name='password_reset_done'),
-        url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+        url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, name='password_reset_confirm'),
         url(r'^reset/done/$', password_reset_complete, name='password_reset_complete'),
 ]
 
